a1d44a302d34457d34ccc064a74804f95934d730
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-09-22 21:50:52 -0400

    going to wait until tomorrow's class to do this

---
+++ C/homework/Assignment1/part2/linked_list.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+struct node {
+	int x;
+	struct node *next;
+};
+
+int main(int argn, char** args) {
+	struct node *root;
+	root = (struct node *) malloc( sizeof(struct node));
+
+}
