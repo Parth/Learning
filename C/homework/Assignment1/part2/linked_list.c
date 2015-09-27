4a3f2d94b40901c0f4008f899db1664d1f83f150
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-09-27 15:04:09 -0400

    adde linked list POC

--- C/homework/Assignment1/part2/linked_list.c
+++ C/homework/Assignment1/part2/linked_list.c
@@ -1,12 +1,20 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 struct node {
 	int x;
 	struct node *next;
 };
 
-int main(int argn, char** args) {
-	struct node *root;
-	root = (struct node *) malloc( sizeof(struct node));
+typedef struct node node;
 
+int main(int argn, char** args) {
+	node *root;
+	root = (node *) malloc(sizeof(node));
+
+	root -> x = 5;
+	root -> next = (node *) malloc(sizeof(node));
+	root -> next -> x = 10;
+	printf("%d -> %d", root -> x, root -> next -> x);
+	return 0;
 }
