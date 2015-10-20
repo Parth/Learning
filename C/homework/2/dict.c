3418fbc881513249e8719d3092b7959e4fd6a768
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-10-20 12:40:14 -0400

    updated

--- C/homework/2/dict.c
+++ C/homework/2/dict.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct node {
+	char* word;
+	int isWord;
+	node* rest;
+	int rest_count;
+}
+typedef struct node node;
+
+struct master {
+	node* nodes;
+	int nodes_count;
+}
+typedef struct master master;
+
 int main(int argn, char** args) {
 	return 0;
 }
