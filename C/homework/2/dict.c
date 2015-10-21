cbffadd49f900eac644d6e2998beb6ddb471fed3
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-10-21 16:07:55 -0400

    i giveup

--- C/homework/2/dict.c
+++ C/homework/2/dict.c
@@ -39,7 +39,7 @@ master* new_master() {
 node* insert_helper(char* word, int level) {
 
 	node* myNode = (node *) malloc(sizeof(node));
-	myNode -> word = (char *) malloc(sizeof(char)*6);
+	myNode -> word = (char *) malloc(sizeof(char)*level+1);
 	int i;
 	for (i = 0; i < level; i++) {
 		myNode -> word[i] = word[i];
@@ -61,23 +61,107 @@ node* insert_helper(char* word, int level) {
 }
 
 void insert(master* root, char* word) {
+	printf("word: %s\n", word);
 	if (root -> nodes_count == 0) {
 		root -> nodes = (node **) malloc(sizeof(node *)*1);
 		root -> nodes_count = 1;
 		root -> nodes[0] = insert_helper(word, 1);
+		return;
 	} else {
-		int i;
-		node* insertion_point = root -> nodes[0];
-		for (i = 0; i < strlen(word); i++) {
-			if (word[i] == insertion_point -> word[i]) {
-				insertion_point = insertion_point -> rest[0];
-			} else {
-				// Between 1'st node, this sucks, our array now has to expand
-				//if (
-				printf("array has to grow\n");
+		int i, j;
+		int match = false;
+		node* insertion_point = NULL;
+		j = 0;
+		for (i = 0; i < root -> nodes_count; i++) {
+			if (word[j] == root -> nodes[i] -> word[j]) {
+				insertion_point = root -> nodes[i];
+				match = true;
+			}
+		}
+		if (!match) {
+			for (i = 0; i < root -> nodes_count; i++) {
+				if (word[j] < root -> nodes[i] -> word[j]) {
+					node** new = (node **) malloc(sizeof(node*) * root -> nodes_count + 1);
+					int k;
+					for (k = 0; k < root -> nodes_count + 1; k++) {
+						if (k < i) {
+							new[k] = (node *) malloc(sizeof(node));
+							new[k] = root -> nodes[k];
+							free(root -> nodes[k]);
+						} else if (k == i) {
+							new[k] = (node *) malloc(sizeof(node));
+							new[k] = insert_helper(word, 1);
+						} else {
+							new[k] = (node *) malloc(sizeof(node));
+							new[k] = root -> nodes[k-1];
+							free(root -> nodes[k-1]);
+						}
+					}
+					free(root -> nodes);
+					root -> nodes = new;
+					root -> nodes_count++;
+					return;
+				}
+			}
+			node** new = (node **) malloc(sizeof(node*) * root -> nodes_count + 1);
+			int k;
+			for (k = 0; k < root -> nodes_count + 1; k++) {
+				new[k] = (node *) malloc(sizeof(node));
+				new[k] = root -> nodes[k];
+				free(root -> nodes[k]);
+			}
+			free(root -> nodes);
+			new[k+1] = insert_helper(word, 1);
+			root -> nodes = new;
+		}
+		//
+		match = false;
+		for (j = 1; j < strlen(word); j++) {
+			for (i = 0; i < insertion_point -> rest_count; i++) {
+				if (word[j] == insertion_point -> rest[i] -> word[j]) {
+					insertion_point = insertion_point -> rest[i];
+					match = true;
+				}
+			}
+			if (!match) {
+				for (i = 0; i < insertion_point -> rest_count; i++) {
+					if (word[j] < insertion_point -> rest[i] -> word[j]) {
+						node** new = (node **) malloc(sizeof(node*) * insertion_point -> rest_count + 1);
+						int k;
+						for (k = 0; k < insertion_point -> rest_count + 1; k++) {
+							if (k < i) {
+								new[k] = (node *) malloc(sizeof(node));
+								new[k] = insertion_point -> rest[k];
+								free(insertion_point -> rest[k]);
+							} else if (k == i) {
+								new[k] = (node *) malloc(sizeof(node));
+								new[k] = insert_helper(word, 1);
+							} else {
+								new[k] = (node *) malloc(sizeof(node));
+								new[k] = insertion_point -> rest[k-1];
+								free(insertion_point -> rest[k-1]);
+							}
+						}
+						free(insertion_point -> rest);
+						insertion_point -> rest = new;
+						root -> nodes_count++;
+						return;
+					}
+				}
+				node** new = (node **) malloc(sizeof(node*) * insertion_point -> rest_count + 1);
+				int k;
+				for (k = 0; k < insertion_point -> rest_count + 1; k++) {
+					new[k] = (node *) malloc(sizeof(node));
+					new[k] = insertion_point -> rest[k];
+					free(insertion_point -> rest[k]);
+				}
+				free(insertion_point -> rest);
+				new[k+1] = insert_helper(word, 1);
+				insertion_point -> rest = new;
 			}
 		}
 	}
+	root -> nodes_count++;
 }
 
 void to_string_helper(node* node, int level) {
@@ -107,7 +191,9 @@ void to_string(master* master) {
 }
 
 int main(int argn, char** args) {
-	node* n = insert_helper("thank god", 1);
-	to_string_helper(n, 1);
+	master* root = new_master();
+	insert(root, "thank god");
+	insert(root, "please");
+	to_string(root);
 	return 0;
 }
