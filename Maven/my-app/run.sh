0738529ca3fb7bc435d7b612e0dcd7890ca2305b
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-11-22 15:46:00 -0500

    added my maven tutorial

---
+++ Maven/my-app/run.sh
@@ -0,0 +1,1 @@
+java -cp target/my-app-1.0-SNAPSHOT.jar com.parth.mehrotra.App
---
+++ Maven/my-app/src/main/java/com/parth/mehrotra/App.java
@@ -0,0 +1,7 @@
+package com.parth.mehrotra;
+
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
