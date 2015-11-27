18eb0bf4ad8f11a96cafa8ed4dcc4d41fada6e61
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-11-27 14:47:48 -0500

    added structure

---
+++ C/homework/4/circuit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define true 1
+#define false 0
+
+int main() {
+
+}
+
+/** Psudeocode: 
+	Variable: {
+		'Name' : char,
+		'Value' : boolean
+	};
+
+	read file
+	create input and output variables
+	parse instructions
+	create temporary variables as nesisary, computing the state of each 
+*/
