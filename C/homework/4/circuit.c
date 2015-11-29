4510283831e5ff732935dd4dc28a145e2f9a3306
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-11-29 14:20:21 -0500

    my psuedocode

--- C/homework/4/circuit.c
+++ C/homework/4/circuit.c
@@ -6,6 +6,25 @@
 #define true 1
 #define false 0
 
-int main() {
-	to_string(NULL, NULL)
+get_nu_of_temp_variables(char** instructions, n_instructions) {
+	for 
+		for
+			if strlen 1
+				if lowercase
+					num++
+
+	return num;
+}
+
+run_program(var** inputs, var** outputs, instructions) {
+	var** temp variables = get_nu_of_temp_variables(instructions);
+	for (instruction) {
+		execute(inputs, outputs, temps, instruction)
+	}
+	print output values
+}
+
+int main(int argc, char** args) {
+	for each input: 
+		run_program(inputs, outputs, instructions)
 }
