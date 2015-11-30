c3e766185c768e7ec5f106a1ae98757f51ddb715
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-11-30 12:31:02 -0500

    nall the logic is done, now just parsing the file

--- C/homework/4/circuit.c
+++ C/homework/4/circuit.c
@@ -6,25 +6,49 @@
 #define true 1
 #define false 0
 
-get_nu_of_temp_variables(char** instructions, n_instructions) {
-	for 
-		for
-			if strlen 1
-				if lowercase
-					num++
+int get_instructions(FILE* instruction_file, char*** instructions) {
+	int lines = 0;
+	char ch;
+	while (!feof(instruction_file)) {
+		ch = fgetc(instruction_file);
+	  	if (ch == '\n') {
+	  		lines++;
+	  	}
+	}
+	rewind(instruction_file);
 
-	return num;
+	instructions[0] = (char **) malloc(lines * sizeof(char *));
+
+	int line_index = 0;
+	char* string = (char *) malloc(256 * sizeof(char *));
+	while (fscanf(instruction_file, "%[^\n]\n", string) == 1) {
+		(instructions[0])[line_index] = string;
+		line_index++;
+		string = (char *) malloc(256 * sizeof(char *));
+	}
+
+	return lines;
 }
 
-run_program(var** inputs, var** outputs, instructions) {
-	var** temp variables = get_nu_of_temp_variables(instructions);
-	for (instruction) {
-		execute(inputs, outputs, temps, instruction)
-	}
-	print output values
+int get_inputVariables(char** instructions, int num_instructions, var*** input_variables) {
+	
+}
+
+int get_temporaryVariables(char** instructions, int num_instructions, var*** temporary_variables) {
+
+}
+
+int get_output_variables(char** instructions, int num_instructions, var*** output_variables) {
+	
 }
 
 int main(int argc, char** args) {
-	for each input: 
-		run_program(inputs, outputs, instructions)
+	FILE* instruction_file = fopen(args[1], "r");
+	char** instructions = NULL;
+	int lines = get_instructions(instruction_file, &instructions);
+	printf("input variables: %d, temporary variables %d, output_variables %d\n", get_inputVariables(
+	fclose(instruction_file);
+
+	FILE* input_file = fopen(args[2], "r");
+	fclose(input_file);
 }
