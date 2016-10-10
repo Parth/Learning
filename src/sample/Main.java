04f5259559af980ea325ab8ed480c0d87e0a39a1
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2016-10-10 15:02:41 -0400

    push

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,53 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        VBox root = new VBox();
+        root.setStyle("-fx-background-color: #336699;");
+
+        HBox top = new HBox();
+        top.setMinHeight(50);
+        top.setMinWidth(600);
+
+        TextField input = new TextField();
+        input.setMinSize(550, 50);
+
+        TextField output = new TextField();
+        Button go = new Button("Go");
+        go.setMinSize(50, 50);
+        go.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String url = input.getText();
+                output.setText(url);
+            }
+        });
+
+        top.getChildren().addAll(input, go);
+
+        output.setPrefHeight(550);
+        output.setPrefWidth(600);
+
+        root.getChildren().addAll(top, output);
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
